cf3d7bafcfaced1f670540198040e5bea0c19c26
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2020-11-25 22:36:29 -0300

    Adição do gh-pages

--- src/components/general/buttomSocial/ButtomSocial.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./buttomSocial.css"
-
-export default props => {
-    return (
-        <a href={props.link} target={"_blank"} rel="noopener noreferrer"><img className="botaoSocial" src={props.logo} alt={"Logo " + props.empresa}></img></a>
-    )
-}
