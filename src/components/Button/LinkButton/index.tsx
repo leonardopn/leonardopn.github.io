51133612f9fafae1c09741e9e36590ec47fba66e
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-01-04 14:08:28 -0300

    feat(Component: LinkButton): :sparkles: Cria o componente

---
+++ src/components/Button/LinkButton/index.tsx
@@ -0,0 +1,55 @@
+import { Link as ChakraLink, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { Icon } from "@iconify/react";
+import Color from "color";
+import { ReactNode } from "react";
+import { Link, LinkProps } from "react-router-dom";
+import { useTheme } from "../../../hooks/useTheme";
+
+interface LinkButtonProps extends ChakraLinkProps {
+	icon?: string;
+	isActive?: boolean;
+	children?: ReactNode;
+	routerProps?: LinkProps;
+	isExternal?: boolean;
+}
+
+export function LinkButton({
+	icon,
+	isActive,
+	children,
+	routerProps,
+	isExternal = false,
+	...rest
+}: LinkButtonProps) {
+	const theme = useTheme();
+
+	return (
+		<ChakraLink
+			as={isExternal ? ChakraLink : Link}
+			display={"flex"}
+			justifyContent={"center"}
+			alignItems={"center"}
+			colorScheme="Background"
+			rel="noopener"
+			_hover={{ background: "Background" }}
+			background={
+				isActive ? "Background" : Color(theme.colors.Background).alpha(0.3).toString()
+			}
+			minW={50}
+			minH={50}
+			borderRadius={10}
+			{...routerProps}
+			{...rest}>
+			{icon && (
+				<Icon
+					icon={icon}
+					width={30}
+					height={30}
+					style={{ marginRight: children ? 10 : 0 }}
+				/>
+			)}
+
+			{children}
+		</ChakraLink>
+	);
+}
