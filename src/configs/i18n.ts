61089b4564a13c2896d4630410157a40d2e1469e
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2023-03-13 22:37:00 -0300

    feat(i18n): :wrench: Cria a implanta i18n

---
+++ src/configs/i18n.ts
@@ -0,0 +1,38 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import { initReactI18next } from "react-i18next";
+import enLocales from "../global/locales/en";
+import ptBRLocales from "../global/locales/pt-br";
+
+export const allLangs = [
+	{
+		label: "Português",
+		value: "ptBR",
+		flag: "br",
+	},
+	{
+		label: "English",
+		value: "en",
+		flag: "us",
+	},
+];
+
+export const defaultLang = allLangs[0];
+
+i18n.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		resources: {
+			en: { translations: enLocales },
+			ptBR: { translations: ptBRLocales },
+		},
+		lng: localStorage.getItem("i18nextLng") || defaultLang.value,
+		debug: false,
+		ns: ["translations"],
+		defaultNS: "translations",
+		interpolation: {
+			escapeValue: false,
+		},
+	});
+
+export default i18n;
---
+++ src/global/locales/pt-br.ts
@@ -0,0 +1,3 @@
+const ptBr = {};
+
+export default ptBr;
